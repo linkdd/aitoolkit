4b664faee3f26ae283525aa921d03d3b8c1561c8
David Delassus <david.jose.delassus@gmail.com>
2024-01-08 12:29:52 +0100

    :white_check_mark: add utility ai test suite

---
+++ tests/utility.spec.cpp
@@ -0,0 +1,75 @@
+#include "doctest.h"
+
+#include "../include/aitoolkit/utility.hpp"
+
+enum class effect {
+  a,
+  b,
+  c,
+};
+
+using namespace aitoolkit::utility;
+struct blackboard_type {
+  effect effect;
+};
+
+TEST_CASE("utility evaluator") {
+
+  class action_a final : public action<blackboard_type> {
+    public:
+      static action_ptr<blackboard_type> make() {
+        return std::make_shared<action_a>();
+      }
+
+      virtual float score(const blackboard_type& blackboard) const override {
+        return 1.0f;
+      }
+
+      virtual void apply(blackboard_type& blackboard) const override {
+        blackboard.effect = effect::a;
+      }
+  };
+
+  class action_b final : public action<blackboard_type> {
+    public:
+      static action_ptr<blackboard_type> make() {
+        return std::make_shared<action_b>();
+      }
+
+      virtual float score(const blackboard_type& blackboard) const override {
+        return 2.0f;
+      }
+
+      virtual void apply(blackboard_type& blackboard) const override {
+        blackboard.effect = effect::b;
+      }
+  };
+
+  class action_c final : public action<blackboard_type> {
+    public:
+      static action_ptr<blackboard_type> make() {
+        return std::make_shared<action_c>();
+      }
+
+      virtual float score(const blackboard_type& blackboard) const override {
+        return 3.0f;
+      }
+
+      virtual void apply(blackboard_type& blackboard) const override {
+        blackboard.effect = effect::c;
+      }
+  };
+
+  SUBCASE("evaluator runs action with highest score") {
+    blackboard_type blackboard;
+    auto machine = evaluator<blackboard_type>{
+      action_a::make(),
+      action_b::make(),
+      action_c::make(),
+    };
+
+    machine.run(blackboard);
+
+    CHECK(blackboard.effect == effect::c);
+  }
+}
